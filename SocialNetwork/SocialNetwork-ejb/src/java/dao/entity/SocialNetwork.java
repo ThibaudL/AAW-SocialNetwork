13af1cc9bee781089206feab0488fed04c266ee7
DSaunier <saunier.denis.86@gmail.com>
2015-01-08 14:05:33 +0100

    SocialNetWork

--- SocialNetwork/SocialNetwork-ejb/src/java/dao/entity/SocialNetwork.java
+++ SocialNetwork/SocialNetwork-ejb/src/java/dao/entity/SocialNetwork.java
@@ -1,30 +1,55 @@
 package dao.entity;
 import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.OneToMany;
 
 public class SocialNetwork {
-	public ArrayList<User> m_users = new ArrayList<User>();
+    @OneToMany(mappedBy = "SocialNetwork")
+    private List<User> m_users;
 
-	public int hashCode() {
-		int lHashCode = 0;
-		if ( this.m_users != null ) {
-			lHashCode += this.m_users.hashCode();
-		}
-		if ( lHashCode == 0 ) {
-			lHashCode = super.hashCode();
-		}
-		return lHashCode;
-	}
+    public SocialNetwork() {
+        this.m_users = new ArrayList<User>();
+    }
 
-	public boolean equals(Object object) {
-		if (this == object) {
-			return true;
-		} else if (object instanceof SocialNetwork) {
-			SocialNetwork lSocialNetworkObject = (SocialNetwork) object;
-			boolean lEquals = true;
-			lEquals &= ((this.m_users == lSocialNetworkObject.m_users)
-				|| (this.m_users != null && this.m_users.equals(lSocialNetworkObject.m_users)));
-			return lEquals;
-		}
-		return false;
-	}
+    public List<User> getUsers() {
+        return m_users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.m_users = users;
+    }
+
+    public boolean addUser(User e) {
+        return m_users.add(e);
+    }
+
+    public boolean removeUser(Object o) {
+        return m_users.remove(o);
+    }
+
+    @Override
+    public int hashCode() {
+            int lHashCode = 0;
+            if ( this.m_users != null ) {
+                    lHashCode += this.m_users.hashCode();
+            }
+            if ( lHashCode == 0 ) {
+                    lHashCode = super.hashCode();
+            }
+            return lHashCode;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+            if (this == object) {
+                    return true;
+            } else if (object instanceof SocialNetwork) {
+                    SocialNetwork lSocialNetworkObject = (SocialNetwork) object;
+                    boolean lEquals = true;
+                    lEquals &= ((this.m_users == lSocialNetworkObject.m_users)
+                            || (this.m_users != null && this.m_users.equals(lSocialNetworkObject.m_users)));
+                    return lEquals;
+            }
+            return false;
+    }
 }
