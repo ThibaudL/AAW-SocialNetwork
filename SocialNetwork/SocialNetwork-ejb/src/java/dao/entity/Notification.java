8fe8b41142cdc9d946ebe6af8fbf716976dd32a6
DSaunier <saunier.denis.86@gmail.com>
2015-01-08 14:01:58 +0100

    Notification

--- SocialNetwork/SocialNetwork-ejb/src/java/dao/entity/Notification.java
+++ SocialNetwork/SocialNetwork-ejb/src/java/dao/entity/Notification.java
@@ -1,56 +1,86 @@
 package dao.entity;
-public class Notification {
-	private int m_id;
-	private String m_content;
-	private boolean m_read;
 
-	public int getId() {
-		return this.m_id;
-	}
+import java.io.Serializable;
+import java.sql.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-	public void setId(int id) {
-		this.m_id = id;
-	}
+@Entity
+public class Notification implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int m_id;
+    @Column
+    private String m_content;
+    @Column
+    private boolean m_read;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date m_dateNotiFication;
+    
+    public int getId() {
+            return this.m_id;
+    }
 
-	public String getContent() {
-		return this.m_content;
-	}
+    public void setId(int id) {
+            this.m_id = id;
+    }
 
-	public void setContent(String content) {
-		this.m_content = content;
-	}
+    public String getContent() {
+            return this.m_content;
+    }
 
-	public boolean getRead() {
-		return this.m_read;
-	}
+    public void setContent(String content) {
+            this.m_content = content;
+    }
 
-	public void setRead(boolean read) {
-		this.m_read = read;
-	}
+    public boolean getRead() {
+            return this.m_read;
+    }
 
-	public int hashCode() {
-		int lHashCode = 0;
-		if ( this.m_content != null ) {
-			lHashCode += this.m_content.hashCode();
-		}
-		if ( lHashCode == 0 ) {
-			lHashCode = super.hashCode();
-		}
-		return lHashCode;
-	}
+    public void setRead(boolean read) {
+            this.m_read = read;
+    }
+    
+    public Date getDateNotiFication() {
+        return this.m_dateNotiFication;
+    }
 
-	public boolean equals(Object object) {
-		if (this == object) {
-			return true;
-		} else if (object instanceof Notification) {
-			Notification lNotificationObject = (Notification) object;
-			boolean lEquals = true;
-			lEquals &= this.m_id == lNotificationObject.m_id;
-			lEquals &= ((this.m_content == lNotificationObject.m_content)
-				|| (this.m_content != null && this.m_content.equals(lNotificationObject.m_content)));
-			lEquals &= this.m_read == lNotificationObject.m_read;
-			return lEquals;
-		}
-		return false;
-	}
+    public void setDateNotiFication(Date dateNotiFication) {
+        this.m_dateNotiFication = dateNotiFication;
+    }
+
+    @Override
+    public int hashCode() {
+            int lHashCode = 0;
+            if ( this.m_content != null ) {
+                    lHashCode += this.m_content.hashCode();
+            }
+            if ( lHashCode == 0 ) {
+                    lHashCode = super.hashCode();
+            }
+            return lHashCode;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+            if (this == object) {
+                    return true;
+            } else if (object instanceof Notification) {
+                    Notification lNotificationObject = (Notification) object;
+                    boolean lEquals = true;
+                    lEquals &= this.m_id == lNotificationObject.m_id;
+                    lEquals &= ((this.m_content == null ? lNotificationObject.m_content == null : this.m_content.equals(lNotificationObject.m_content))
+                            || (this.m_content != null && this.m_content.equals(lNotificationObject.m_content)));
+                    lEquals &= this.m_read == lNotificationObject.m_read;
+                    return lEquals;
+            }
+            return false;
+    }
+    
+    
 }
