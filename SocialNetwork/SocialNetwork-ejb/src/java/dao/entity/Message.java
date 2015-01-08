9f9fd61e8c75a50c16a1cd7d5545af0ee6b9134e
ThibaudL <lamarchethibaud@hotmail.fr>
2015-01-08 17:15:05 +0100

    Added tables name and removed social Network class

--- SocialNetwork/SocialNetwork-ejb/src/java/dao/entity/Message.java
+++ SocialNetwork/SocialNetwork-ejb/src/java/dao/entity/Message.java
@@ -13,10 +13,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
+@Table(name = "Messages")
 public class Message implements Serializable {
     
     @Id
