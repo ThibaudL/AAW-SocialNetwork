e0dd7a81e4c3f394269abe1c02c1494cc01cb1a7
ThibaudL <lamarchethibaud@hotmail.fr>
2015-02-05 10:52:53 +0100

    Added image management

--- SocialNetwork/SocialNetwork-war/src/java/beans/ProfileMB.java
+++ SocialNetwork/SocialNetwork-war/src/java/beans/ProfileMB.java
@@ -7,26 +7,27 @@ package beans;
 
 import dao.entity.Profile;
 import dao.entity.User;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.ParseException;
-import java.util.Date;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.Part;
+import javax.faces.event.PhaseId;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import org.primefaces.model.UploadedFile;
 import service.ProfileServiceLocal;
 import service.UserServiceLocal;
 import utils.SessionUtils;
-
 /**
  *
  * @author Thibaud
@@ -38,8 +39,9 @@ public class ProfileMB implements Serializable{
     private Profile profile;
     
     private String birthdayString;
-    private Part profilePictureFile;
-    
+    private UploadedFile profilePictureFile;
+    private StreamedContent readableProfilePicture;
+   
     
     @EJB
     ProfileServiceLocal profileService;
@@ -57,12 +59,22 @@ public class ProfileMB implements Serializable{
         return birthdayString;
     }
 
-    public Part getProfilePictureFile() {
+    public UploadedFile getProfilePictureFile() {
         return profilePictureFile;
     }
 
-    public void setProfilePictureFile(Part profilePictureFile) {
+    public void setProfilePictureFile(UploadedFile profilePictureFile) {
         this.profilePictureFile = profilePictureFile;
+    }
+    
+    private StreamedContent initImage() throws IOException {
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+            // So, we're rendering the HTML. Return a stub StreamedContent so that it will generate right URL.
+            return new DefaultStreamedContent();
+        } else {
+            return new DefaultStreamedContent(new ByteArrayInputStream(profile.getPicture()));
+        }
     }
     
     
@@ -86,16 +98,34 @@ public class ProfileMB implements Serializable{
         return "home.xhtml";
     }
     
-    private void upload(){
+    public void upload(){
         try {
+            /*
             if(profilePictureFile != null)
-                profile.setPicture(new Scanner(profilePictureFile.getInputStream())
-                    .useDelimiter("\\A").next().getBytes());
+            profile.setPicture(profilePictureFile.getContents());
+            */
+            FacesMessage msg = new FacesMessage("Success! ", profilePictureFile.getFileName() + " is uploaded.");
+            
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+            Logger.getLogger(ProfileMB.class.getName()).log(Level.SEVERE, "DEBUG DEBUG DEBUG : "+profilePictureFile.getFileName() + " - "+profilePictureFile.getInputstream() + " - "+profilePictureFile.getSize());
+           
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = profilePictureFile.getInputstream();
+            byte[] buffer = new byte[1024];
+            while (true) {
+                int r = in.read(buffer);
+                if (r == -1) break;
+                out.write(buffer, 0, r);
+            }
+
+            profile.setPicture(out.toByteArray());
         } catch (IOException ex) {
             Logger.getLogger(ProfileMB.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
 
+ 
     public Profile getProfile() {
         return profile;
     }
@@ -103,6 +133,22 @@ public class ProfileMB implements Serializable{
     public void setProfile(Profile profile) {
         this.profile = profile;
     }
+
+    public StreamedContent getReadableProfilePicture() {
+        try {
+            readableProfilePicture = initImage();
+        } catch (IOException ex) {
+            Logger.getLogger(ProfileMB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return readableProfilePicture;
+    }
+
+    public void setReadableProfilePicture(StreamedContent readableProfilePicture) {
+        this.readableProfilePicture = readableProfilePicture;
+    }
+     
+    
+    
     
     
 
