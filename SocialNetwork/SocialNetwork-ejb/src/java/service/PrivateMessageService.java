63c0dfeef8f0b694990dec1079d300891f9979ed
DSaunier <saunier.denis.86@gmail.com>
2015-02-23 14:28:35 +0100

    Add notification for private message

--- SocialNetwork/SocialNetwork-ejb/src/java/service/PrivateMessageService.java
+++ SocialNetwork/SocialNetwork-ejb/src/java/service/PrivateMessageService.java
@@ -7,7 +7,10 @@ package service;
 
 import dao.entity.Comment;
 import dao.entity.Message;
+import dao.entity.Notification;
 import dao.entity.PrivateMessage;
+import dao.entity.Profile;
+import dao.entity.User;
 import dao.impl.NotificationFacadeLocal;
 import dao.impl.PictureFacadeLocal;
 import dao.impl.PrivateMessageFacadeLocal;
@@ -17,6 +20,7 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateful;
 import javax.ejb.Stateless;
+import websocket.SocketMediator;
 
 /**
  *
@@ -46,6 +50,7 @@ public class PrivateMessageService implements PrivateMessageServiceLocal {
             pm.setContent(content);
             pm.setDate(new Date());
             privateMessageFacade.create(pm);
+            sendNotification(authorId, userId, content, pm.getId());
         }
         return pm.getId();
     }
@@ -61,13 +66,28 @@ public class PrivateMessageService implements PrivateMessageServiceLocal {
         mComment.setDate(new Date());
         pm.comments.add(comment);
         privateMessageFacade.edit(pm);
+        sendNotification(authorId, userId, content, pm.getId());
     }
     
+    @Override
     public List<PrivateMessage> findConversation(Integer userId){
         return privateMessageFacade.findConversation(userId);
     }
     
+    @Override
     public PrivateMessage getConversation(Integer msgId, Integer userId){
         return privateMessageFacade.getConversationById(msgId, userId);
+    }
+    
+    private void sendNotification(Integer authorId, Integer userId, String content, Integer msgId){
+        User user = userFacade.find(authorId);
+        Notification not= new Notification();
+        Profile p = user.getProfile();
+        not.setContent(p.getFirstname()+" "+ p.getLastname() +" sent a private message : "+content);
+        not.setDate(new Date());
+        not.setLink("http://localhost:8080/SocialNetwork-war/faces/privateMessage.xhtml?conversationId="+msgId);
+        not.setUser(userFacade.find(userId));
+        notificationFacade.create(not);
+        SocketMediator.sendNotification(not);
     }
 }
